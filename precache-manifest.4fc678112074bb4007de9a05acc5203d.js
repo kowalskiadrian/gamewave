de64402a5a74b2f11beaf3e8252ae9f378f59079
kowalskiadrian <kowalski.adrian.adam@outlook.com>
2020-09-07 13:47:24 +0200

    Updates

---
+++ precache-manifest.4fc678112074bb4007de9a05acc5203d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06396125a4db04e831f6fb1b4d3bec41",
+    "url": "/gamewave/index.html"
+  },
+  {
+    "revision": "9dd82fd719af3bf64b31",
+    "url": "/gamewave/static/css/main.b42a0614.chunk.css"
+  },
+  {
+    "revision": "7e195202ea1acfb3dbde",
+    "url": "/gamewave/static/js/2.7b955c02.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gamewave/static/js/2.7b955c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd82fd719af3bf64b31",
+    "url": "/gamewave/static/js/main.eb691d85.chunk.js"
+  },
+  {
+    "revision": "1d227797b5e01580de04",
+    "url": "/gamewave/static/js/runtime-main.3267022e.js"
+  },
+  {
+    "revision": "2433d126ac2ad91ff6daa51c165e7845",
+    "url": "/gamewave/static/media/Bishop-white.2433d126.svg"
+  },
+  {
+    "revision": "58d796b2d2f9cd1f48ce2220748bf4cd",
+    "url": "/gamewave/static/media/Bishop.58d796b2.svg"
+  },
+  {
+    "revision": "ad1c545958358a33b96006e50cf19132",
+    "url": "/gamewave/static/media/King-white.ad1c5459.svg"
+  },
+  {
+    "revision": "912b64b4ab59093dd3aef41c37416d45",
+    "url": "/gamewave/static/media/King.912b64b4.svg"
+  },
+  {
+    "revision": "ce2faf3cfb7225b82931b675a927877a",
+    "url": "/gamewave/static/media/Knight-white.ce2faf3c.svg"
+  },
+  {
+    "revision": "8b60a74faf3ec05ece5b743a73e35170",
+    "url": "/gamewave/static/media/Knight.8b60a74f.svg"
+  },
+  {
+    "revision": "b24dced7e782e0302aa93089e4d0ef6a",
+    "url": "/gamewave/static/media/Pawn-white.b24dced7.svg"
+  },
+  {
+    "revision": "b3be38d2662ff87c690a6a738567372e",
+    "url": "/gamewave/static/media/Pawn.b3be38d2.svg"
+  },
+  {
+    "revision": "3ac262dacc036de22e65da848ea51653",
+    "url": "/gamewave/static/media/Queen-white.3ac262da.svg"
+  },
+  {
+    "revision": "6a1f3004d49012cdd40989112401b1cd",
+    "url": "/gamewave/static/media/Queen.6a1f3004.svg"
+  },
+  {
+    "revision": "3dd20b2a278b807ceedf2bba06a68ea0",
+    "url": "/gamewave/static/media/Rook-white.3dd20b2a.svg"
+  },
+  {
+    "revision": "9b12af1b08649c62c4341993efbe345f",
+    "url": "/gamewave/static/media/Rook.9b12af1b.svg"
+  },
+  {
+    "revision": "8402ea0b0924f4530c4af2e493375a51",
+    "url": "/gamewave/static/media/arrow-button.8402ea0b.svg"
+  },
+  {
+    "revision": "0578369b4983324ee63d4048930f9262",
+    "url": "/gamewave/static/media/background.0578369b.svg"
+  },
+  {
+    "revision": "5efbbd1d3ac99761634bcdd94a8d6af0",
+    "url": "/gamewave/static/media/chess-icon.5efbbd1d.svg"
+  },
+  {
+    "revision": "c0ecc0ed264d1ec2f4cc6db377d97347",
+    "url": "/gamewave/static/media/chess-title.c0ecc0ed.svg"
+  },
+  {
+    "revision": "57f899a9e8b9c14a89f42468ddb1a80b",
+    "url": "/gamewave/static/media/rubik-icon.57f899a9.svg"
+  },
+  {
+    "revision": "740139a952d0ed2fa2dcadaa2952e33a",
+    "url": "/gamewave/static/media/rubiks-title.740139a9.svg"
+  },
+  {
+    "revision": "834ae074c8209dedea26e08247975278",
+    "url": "/gamewave/static/media/space-buttom.834ae074.svg"
+  },
+  {
+    "revision": "a56384ab78838c08230c60b22c46cc22",
+    "url": "/gamewave/static/media/tetris-icon.a56384ab.svg"
+  },
+  {
+    "revision": "b06d3068330bdc7868dcacd69e15d460",
+    "url": "/gamewave/static/media/tetris-title.b06d3068.svg"
+  }
+]);
